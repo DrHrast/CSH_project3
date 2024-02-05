e37fee30ee6beed92a347a59f352c4812394aa73
DrHrast <petar.huljek@gmail.com>
2024-02-05 21:07:42 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace PH_project3
+{
+    partial class PH_project
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tabPH_project = new System.Windows.Forms.TabControl();
+            this.tabPhProject = new System.Windows.Forms.TabPage();
+            this.tabSpProject = new System.Windows.Forms.TabPage();
+            this.checkKeepDataBox = new System.Windows.Forms.CheckBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.datumBox = new System.Windows.Forms.DateTimePicker();
+            this.addButton = new System.Windows.Forms.Button();
+            this.spolBox = new System.Windows.Forms.ComboBox();
+            this.mjestoBox = new System.Windows.Forms.TextBox();
+            this.drzavaBox = new System.Windows.Forms.TextBox();
+            this.prezimeBox = new System.Windows.Forms.TextBox();
+            this.imeBox = new System.Windows.Forms.TextBox();
+            this.mjestoLabel = new System.Windows.Forms.Label();
+            this.drzavaLabel = new System.Windows.Forms.Label();
+            this.datumLabel = new System.Windows.Forms.Label();
+            this.spolLabel = new System.Windows.Forms.Label();
+            this.prezimeLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.dataPersonView = new System.Windows.Forms.DataGridView();
+            this.tabPH_project.SuspendLayout();
+            this.tabPhProject.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPersonView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabPH_project
+            // 
+            this.tabPH_project.Controls.Add(this.tabPhProject);
+            this.tabPH_project.Controls.Add(this.tabSpProject);
+            this.tabPH_project.Location = new System.Drawing.Point(13, 13);
+            this.tabPH_project.Name = "tabPH_project";
+            this.tabPH_project.SelectedIndex = 0;
+            this.tabPH_project.Size = new System.Drawing.Size(918, 539);
+            this.tabPH_project.TabIndex = 0;
+            // 
+            // tabPhProject
+            // 
+            this.tabPhProject.Controls.Add(this.checkKeepDataBox);
+            this.tabPhProject.Controls.Add(this.progressBar);
+            this.tabPhProject.Controls.Add(this.datumBox);
+            this.tabPhProject.Controls.Add(this.addButton);
+            this.tabPhProject.Controls.Add(this.spolBox);
+            this.tabPhProject.Controls.Add(this.mjestoBox);
+            this.tabPhProject.Controls.Add(this.drzavaBox);
+            this.tabPhProject.Controls.Add(this.prezimeBox);
+            this.tabPhProject.Controls.Add(this.imeBox);
+            this.tabPhProject.Controls.Add(this.mjestoLabel);
+            this.tabPhProject.Controls.Add(this.drzavaLabel);
+            this.tabPhProject.Controls.Add(this.datumLabel);
+            this.tabPhProject.Controls.Add(this.spolLabel);
+            this.tabPhProject.Controls.Add(this.prezimeLabel);
+            this.tabPhProject.Controls.Add(this.nameLabel);
+            this.tabPhProject.Controls.Add(this.dataPersonView);
+            this.tabPhProject.Location = new System.Drawing.Point(4, 22);
+            this.tabPhProject.Name = "tabPhProject";
+            this.tabPhProject.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPhProject.Size = new System.Drawing.Size(910, 513);
+            this.tabPhProject.TabIndex = 0;
+            this.tabPhProject.Text = "Php";
+            this.tabPhProject.UseVisualStyleBackColor = true;
+            // 
+            // tabSpProject
+            // 
+            this.tabSpProject.Location = new System.Drawing.Point(4, 22);
+            this.tabSpProject.Name = "tabSpProject";
+            this.tabSpProject.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSpProject.Size = new System.Drawing.Size(910, 513);
+            this.tabSpProject.TabIndex = 1;
+            this.tabSpProject.Text = "Spp";
+            this.tabSpProject.UseVisualStyleBackColor = true;
+            // 
+            // checkKeepDataBox
+            // 
+            this.checkKeepDataBox.AutoSize = true;
+            this.checkKeepDataBox.Location = new System.Drawing.Point(35, 375);
+            this.checkKeepDataBox.Name = "checkKeepDataBox";
+            this.checkKeepDataBox.Size = new System.Drawing.Size(77, 17);
+            this.checkKeepDataBox.TabIndex = 35;
+            this.checkKeepDataBox.Text = "Keep Data";
+            this.checkKeepDataBox.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.Color.Lime;
+            this.progressBar.Location = new System.Drawing.Point(34, 351);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar.Maximum = 102;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(142, 19);
+            this.progressBar.Step = 17;
+            this.progressBar.TabIndex = 34;
+            // 
+            // datumBox
+            // 
+            this.datumBox.CustomFormat = "dd,MM,yyyy";
+            this.datumBox.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datumBox.Location = new System.Drawing.Point(78, 216);
+            this.datumBox.Margin = new System.Windows.Forms.Padding(2);
+            this.datumBox.Name = "datumBox";
+            this.datumBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.datumBox.Size = new System.Drawing.Size(102, 20);
+            this.datumBox.TabIndex = 30;
+            this.datumBox.Value = new System.DateTime(2023, 12, 5, 0, 0, 0, 0);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(34, 394);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(143, 23);
+            this.addButton.TabIndex = 33;
+            this.addButton.Text = "Dodaj osobu";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // spolBox
+            // 
+            this.spolBox.FormattingEnabled = true;
+            this.spolBox.Location = new System.Drawing.Point(78, 156);
+            this.spolBox.Name = "spolBox";
+            this.spolBox.Size = new System.Drawing.Size(47, 21);
+            this.spolBox.TabIndex = 29;
+            // 
+            // mjestoBox
+            // 
+            this.mjestoBox.Location = new System.Drawing.Point(78, 303);
+            this.mjestoBox.Name = "mjestoBox";
+            this.mjestoBox.Size = new System.Drawing.Size(100, 20);
+            this.mjestoBox.TabIndex = 32;
+            this.mjestoBox.Validated += new System.EventHandler(this.mjestoBox_Validated);
+            // 
+            // drzavaBox
+            // 
+            this.drzavaBox.Location = new System.Drawing.Point(78, 249);
+            this.drzavaBox.Name = "drzavaBox";
+            this.drzavaBox.Size = new System.Drawing.Size(100, 20);
+            this.drzavaBox.TabIndex = 31;
+            this.drzavaBox.Validated += new System.EventHandler(this.drzavaBox_Validated);
+            // 
+            // prezimeBox
+            // 
+            this.prezimeBox.Location = new System.Drawing.Point(78, 122);
+            this.prezimeBox.Name = "prezimeBox";
+            this.prezimeBox.Size = new System.Drawing.Size(100, 20);
+            this.prezimeBox.TabIndex = 28;
+            this.prezimeBox.Validated += new System.EventHandler(this.prezimeBox_Validated);
+            // 
+            // imeBox
+            // 
+            this.imeBox.Location = new System.Drawing.Point(78, 87);
+            this.imeBox.Name = "imeBox";
+            this.imeBox.Size = new System.Drawing.Size(100, 20);
+            this.imeBox.TabIndex = 27;
+            this.imeBox.Validated += new System.EventHandler(this.imeBox_Validated);
+            // 
+            // mjestoLabel
+            // 
+            this.mjestoLabel.AutoSize = true;
+            this.mjestoLabel.Location = new System.Drawing.Point(32, 287);
+            this.mjestoLabel.Name = "mjestoLabel";
+            this.mjestoLabel.Size = new System.Drawing.Size(79, 13);
+            this.mjestoLabel.TabIndex = 26;
+            this.mjestoLabel.Text = "MjestoRođenja";
+            // 
+            // drzavaLabel
+            // 
+            this.drzavaLabel.AutoSize = true;
+            this.drzavaLabel.Location = new System.Drawing.Point(32, 256);
+            this.drzavaLabel.Name = "drzavaLabel";
+            this.drzavaLabel.Size = new System.Drawing.Size(41, 13);
+            this.drzavaLabel.TabIndex = 25;
+            this.drzavaLabel.Text = "Država";
+            // 
+            // datumLabel
+            // 
+            this.datumLabel.AutoSize = true;
+            this.datumLabel.Location = new System.Drawing.Point(32, 200);
+            this.datumLabel.Name = "datumLabel";
+            this.datumLabel.Size = new System.Drawing.Size(79, 13);
+            this.datumLabel.TabIndex = 24;
+            this.datumLabel.Text = "DatumRođenja";
+            // 
+            // spolLabel
+            // 
+            this.spolLabel.AutoSize = true;
+            this.spolLabel.Location = new System.Drawing.Point(32, 164);
+            this.spolLabel.Name = "spolLabel";
+            this.spolLabel.Size = new System.Drawing.Size(28, 13);
+            this.spolLabel.TabIndex = 23;
+            this.spolLabel.Text = "Spol";
+            // 
+            // prezimeLabel
+            // 
+            this.prezimeLabel.AutoSize = true;
+            this.prezimeLabel.Location = new System.Drawing.Point(32, 129);
+            this.prezimeLabel.Name = "prezimeLabel";
+            this.prezimeLabel.Size = new System.Drawing.Size(44, 13);
+            this.prezimeLabel.TabIndex = 22;
+            this.prezimeLabel.Text = "Prezime";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(32, 94);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(24, 13);
+            this.nameLabel.TabIndex = 21;
+            this.nameLabel.Text = "Ime";
+            // 
+            // dataPersonView
+            // 
+            this.dataPersonView.AllowUserToAddRows = false;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataPersonView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataPersonView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.Format = "dd/MM/yyyy";
+            dataGridViewCellStyle6.NullValue = null;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataPersonView.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataPersonView.Location = new System.Drawing.Point(203, 59);
+            this.dataPersonView.Name = "dataPersonView";
+            this.dataPersonView.ReadOnly = true;
+            this.dataPersonView.Size = new System.Drawing.Size(676, 395);
+            this.dataPersonView.TabIndex = 20;
+            // 
+            // PH_project
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 564);
+            this.Controls.Add(this.tabPH_project);
+            this.Name = "PH_project";
+            this.Text = "PH_project";
+            this.tabPH_project.ResumeLayout(false);
+            this.tabPhProject.ResumeLayout(false);
+            this.tabPhProject.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPersonView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabPH_project;
+        private System.Windows.Forms.TabPage tabPhProject;
+        private System.Windows.Forms.TabPage tabSpProject;
+        private System.Windows.Forms.CheckBox checkKeepDataBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.DateTimePicker datumBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ComboBox spolBox;
+        private System.Windows.Forms.TextBox mjestoBox;
+        private System.Windows.Forms.TextBox drzavaBox;
+        private System.Windows.Forms.TextBox prezimeBox;
+        private System.Windows.Forms.TextBox imeBox;
+        private System.Windows.Forms.Label mjestoLabel;
+        private System.Windows.Forms.Label drzavaLabel;
+        private System.Windows.Forms.Label datumLabel;
+        private System.Windows.Forms.Label spolLabel;
+        private System.Windows.Forms.Label prezimeLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.DataGridView dataPersonView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PH_project3
+{
+    public partial class PH_project : Form
+    {
+
+        class Person
+        {
+            public string Ime { get; set; }
+            public string Prezime { get; set; }
+            public char Spol { get; set; }
+            public DateTime DatumRođenja { get; set; }
+            public string Država { get; set; }
+            public string MjestoRođenja { get; set; }
+        }
+
+        ObservableCollection<Person> ListOfPersons = new ObservableCollection<Person>()
+        { new Person {
+            Ime = "Peter",
+            Prezime = "Falk",
+            Spol = 'M',
+            DatumRođenja = new DateTime(1927, 9, 16),
+            Država = "SAD",
+            MjestoRođenja = "New York" },
+        new Person {
+            Ime = "Antonio",
+            Prezime = "Banderas",
+            Spol = 'M',
+            DatumRođenja = new DateTime(1960, 8, 10),
+            Država = "Španjolska",
+            MjestoRođenja = "Malaga" },
+        new Person {
+            Ime = "Tomislav",
+            Prezime = "Trifunović",
+            Spol = 'M',
+            DatumRođenja = new DateTime(1947, 2, 22),
+            Država = "Srbija",
+            MjestoRođenja = "Mali Popović" },
+        new Person {
+            Ime = "Sonja",
+            Prezime = "Kovač",
+            Spol = 'F',
+            DatumRođenja = new DateTime(1984, 6, 18),
+            Država = "Hrvatska",
+            MjestoRođenja = "Bjelovar" },
+        new Person {
+            Ime = "Michael",
+            Prezime = "Kane",
+            Spol = 'M',
+            DatumRođenja = new DateTime(1933, 3, 14),
+            Država = "Engleska",
+            MjestoRođenja = "London" },
+        new Person {
+            Ime = "Michelle",
+            Prezime = "Pfeiffer",
+            Spol = 'F',
+            DatumRođenja = new DateTime(1958, 4, 29),
+            Država = "SAD",
+            MjestoRođenja = "Santa Ana" },
+        new Person {
+            Ime = "Milla",
+            Prezime = "Jovovich",
+            Spol = 'F',
+            DatumRođenja = new DateTime(1975, 12, 17),
+            Država = "Ukrajina",
+            MjestoRođenja = "Kijev" },
+        new Person {
+            Ime = "Keanu",
+            Prezime = "Reeves",
+            Spol = 'M',
+            DatumRođenja = new DateTime(1964, 9, 2),
+            Država = "Lebanon",
+            MjestoRođenja = "Beirut" },
+        new Person {
+            Ime = "Robert",
+            Prezime = "De Niro",
+            Spol = 'M',
+            DatumRođenja = new DateTime(1943, 8, 17),
+            Država = "SAD",
+            MjestoRođenja = "New York" }
+        };
+
+        //DID_IT - 03 Try adding tabs for possible other projects.
+        //TODO - 03.1 Tabs added, just figure out how to implement it (could be with new project or something else).
+        public PH_project()
+        {
+            InitializeComponent();
+
+            dataPersonView.DataSource = ListOfPersons;
+
+            spolBox.Items.Add('M');
+            spolBox.Items.Add('F');
+            spolBox.SelectedIndex = 0;
+            progressBar.Value = 34;
+            datumBox.Format = DateTimePickerFormat.Custom;
+            datumBox.CustomFormat = "dd/MM/yyyy";
+        }
+
+        //DID_IT - 02 Finally get to the POINT and try list sorting.
+        //DID_IT - 02.1 Sort works!! Now how to make individual column to sort by it self?
+        //DID_IT - 02.2 Maybe some other sort of check, like a switch.
+        bool IsColumnSorted = false;
+        private void dataPersonView_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            var temp = new List<Person>();
+            if (!IsColumnSorted)
+            {
+                switch (e.ColumnIndex)
+                {
+                    case 0:
+                        temp = ListOfPersons.OrderBy(x => x.Ime).ToList(); break;
+                    case 1:
+                        temp = ListOfPersons.OrderBy(x => x.Prezime).ToList(); break;
+                    case 2:
+                        temp = ListOfPersons.OrderBy(x => x.Spol).ToList(); break;
+                    case 3:
+                        temp = ListOfPersons.OrderBy(x => x.DatumRođenja).ToList(); break;
+                    case 4:
+                        temp = ListOfPersons.OrderBy(x => x.Država).ToList(); break;
+                    case 5:
+                        temp = ListOfPersons.OrderBy(x => x.MjestoRođenja).ToList(); break;
+                }
+                ListOfPersons = new ObservableCollection<Person>(temp);
+                IsColumnSorted = true;
+
+            }
+            else if (IsColumnSorted)
+            {
+                switch (e.ColumnIndex)
+                {
+                    case 0:
+                        temp = ListOfPersons.OrderByDescending(x => x.Ime).ToList(); break;
+                    case 1:
+                        temp = ListOfPersons.OrderByDescending(x => x.Prezime).ToList(); break;
+                    case 2:
+                        temp = ListOfPersons.OrderByDescending(x => x.Spol).ToList(); break;
+                    case 3:
+                        temp = ListOfPersons.OrderByDescending(x => x.DatumRođenja).ToList(); break;
+                    case 4:
+                        temp = ListOfPersons.OrderByDescending(x => x.Država).ToList(); break;
+                    case 5:
+                        temp = ListOfPersons.OrderByDescending(x => x.MjestoRođenja).ToList(); break;
+                }
+                ListOfPersons = new ObservableCollection<Person>(temp);
+                IsColumnSorted = false;
+
+            }
+            //ListOfPersons = (BindingList<Person>)ListOfPersons.OrderBy(x => x.Ime);
+            //dataPersonView.DataSource = null;
+            dataPersonView.DataSource = ListOfPersons;
+        }
+
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (progressBar.Value >= 100)
+            {
+                //Za ObservableCollection mora biti postavljen DataSource = null, pa ponovno DataSource = listOf... 
+                ListOfPersons.Add(new Person
+                {
+                    Ime = imeBox.Text,
+                    Prezime = prezimeBox.Text,
+                    Spol = (char)spolBox.SelectedItem,
+                    DatumRođenja = datumBox.Value,
+                    Država = drzavaBox.Text,
+                    MjestoRođenja = mjestoBox.Text,
+                });
+
+                if (!checkKeepDataBox.Checked)
+                {
+                    progressBar.Value = 34;
+                    imeBox.Text = string.Empty;
+                    prezimeBox.Text = string.Empty;
+                    drzavaBox.Text = string.Empty;
+                    mjestoBox.Text = string.Empty;
+                    IsImeEmpty = true;
+                    IsPrezimeEmpty = true;
+                    IsDrzavaEmpty = true;
+                    IsMjestoEmpty = true;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Fill all data boxes.", "Fill error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            dataPersonView.DataSource = null;
+            dataPersonView.DataSource = ListOfPersons;
+        }
+
+        bool IsImeEmpty = true;
+        //DID_IT - 01 Make a bool for {value != ""} to fix a repeted progress bar fill.
+        //DID_IT - 01.1 Some bug with bar emptying when a field is empty and field is changed.
+        //DID_IT - 01.2 Another bug after adding value. Can't add another and 
+        private void imeBox_Validated(object sender, EventArgs e)
+        {
+            if (imeBox.Text.Length > 0 && IsImeEmpty)
+            {
+                progressBar.Value += 17;
+                IsImeEmpty = false;
+            }
+            else if (imeBox.Text == "" && progressBar.Value != 0 && !IsImeEmpty)
+            {
+                progressBar.Value -= 17;
+                IsImeEmpty = true;
+            }
+        }
+        bool IsPrezimeEmpty = true;
+        private void prezimeBox_Validated(object sender, EventArgs e)
+        {
+            if (prezimeBox.Text.Length > 0 && IsPrezimeEmpty)
+            {
+                progressBar.Value += 17;
+                IsPrezimeEmpty = false;
+            }
+            else if (prezimeBox.Text == "" && progressBar.Value != 0 && !IsPrezimeEmpty)
+            {
+                progressBar.Value -= 17;
+                IsPrezimeEmpty = true;
+            }
+        }
+        bool IsDrzavaEmpty = true;
+        private void drzavaBox_Validated(object sender, EventArgs e)
+        {
+            if (drzavaBox.Text.Length > 0 && IsDrzavaEmpty)
+            {
+                progressBar.Value += 17;
+                IsDrzavaEmpty = false;
+            }
+            else if (drzavaBox.Text == "" && progressBar.Value != 0 && !IsDrzavaEmpty)
+            {
+                progressBar.Value -= 17;
+                IsDrzavaEmpty = true;
+            }
+        }
+        bool IsMjestoEmpty = true;
+        private void mjestoBox_Validated(object sender, EventArgs e)
+        {
+            if (mjestoBox.Text.Length > 0 && IsMjestoEmpty)
+            {
+                progressBar.Value += 17;
+                IsMjestoEmpty = false;
+            }
+            else if (mjestoBox.Text == "" && progressBar.Value != 0 && !IsMjestoEmpty)
+            {
+                progressBar.Value -= 17;
+                IsMjestoEmpty = true;
+            }
+        }
+
+    }
+}
